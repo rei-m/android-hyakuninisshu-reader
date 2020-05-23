3880dc8f84a078abd2b582de46355324be9dbf7b
rei-m <star.oceans.rm@gmail.com>
2020-05-24 02:23:03 +0900

    add core dependencies

--- app/src/main/java/net/hyakuninanki/reader/App.kt
+++ app/src/main/java/net/hyakuninanki/reader/App.kt
@@ -1,6 +1,27 @@
 package net.hyakuninanki.reader
 
 import android.app.Application
+import timber.log.Timber
 
 open class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+//        initAdMob()
+    }
+
+    protected open fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+//            Timber.plant(CrashlyticsTree())
+        }
+    }
+
+//    protected open fun initAdMob() {
+//        MobileAds.initialize(this) {
+//            Timber.i("initialize Ad")
+//        }
+//    }
 }
