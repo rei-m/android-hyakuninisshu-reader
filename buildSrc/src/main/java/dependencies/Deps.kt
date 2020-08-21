77749409b73d77b2780fc7671c85305421276e71
rei-m <star.oceans.rm@gmail.com>
2020-08-21 23:05:54 +0900

    refactor test dependencies

--- buildSrc/src/main/java/dependencies/Deps.kt
+++ buildSrc/src/main/java/dependencies/Deps.kt
@@ -18,11 +18,50 @@
 package dependencies
 
 object Deps {
+    object Kotlin {
+        private const val version = "1.4.0"
+        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val kotlinStdlibJdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
+        const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
+    }
+
+    object AndroidX {
+        object Arch {
+            const val coreTesting = "androidx.arch.core:core-testing:2.1.0"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+            const val junit = "androidx.test.ext:junit:1.1.1"
+
+            object Espresso {
+                private const val version = "3.2.0"
+                const val core = "androidx.test.espresso:espresso-core:${version}"
+                const val intents = "androidx.test.espresso:espresso-intents:${version}"
+            }
+        }
+    }
+
     object Dagger {
         private const val version = "2.28-alpha"
         const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
         const val hiltAndroid = "com.google.dagger:hilt-android:$version"
         const val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:$version"
         const val hiltAndroidTesting = "com.google.dagger:hilt-android-testing:$version"
+    }
+
+    object AssertJ {
+        const val core = "org.assertj:assertj-core:3.16.1"
+    }
+
+    object Robolectric {
+        const val robolectric = "org.robolectric:robolectric:4.3.1"
+    }
+
+    object MockitoKotlin2 {
+        const val mockitokotlin2 = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
     }
 }
